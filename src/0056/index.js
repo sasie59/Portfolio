85e337352d643794b47c51688c74cc7891a812e7
sasie59 <sasie59@gmail.com>
2022-03-04 01:57:26 +0800

    內容先上 ，還差turn off  & on

--- src/0056/index.js
+++ src/0056/index.js
@@ -2,8 +2,14 @@ import React, { useState } from 'react';
 import stock from './stock.jpg';
 import "./App.scss";
 
+const cashDividend = 1.80;
+const fundSize = 1.2 * 100000000000;
+const totalUnit = 3496534 * 1000;
+const etfS = (fundSize / totalUnit).toFixed(2); //ETF淨值
+const yearRateOfReturn = 10.20;
+
 export default function Index() {
-  const [twStock, setTwStock] = useState(33.33);
+  const [twStock, setTwStock] = useState(34.22);
   let today = new Date();
   return (
     <div className='layout'>
@@ -19,16 +25,21 @@ export default function Index() {
       </header>
       <div className='nav'>
         <button>現金股利</button>
-        <button>
-          累計發放股利</button>
-        <span>(近10年)</span>
         <button>基金規模</button>
+        <button>總單位數</button>
         <button>折溢價幅度</button>
         <button>殖利率</button>
         <button>年報酬率</button>
       </div>
-      <div className='result'>result</div>
-      <img className='img' src={stock} alt="" />
+      <div name='cashDividend' className='result'>{cashDividend}元</div>
+      <div name='fundSize' className='result'>{fundSize}元</div>
+      <div name='totalUnit' className='result'>{totalUnit} 股</div>
+      <div name='etfS' className='result'>
+        {(((twStock - etfS)).toFixed(2) / twStock * 100).toFixed(2)}%
+      </div>
+      <div name='yield' className='result'>{((cashDividend / twStock) * 100).toFixed(2)}%</div>
+      <div name='yearRateOfReturn' className='result'>{yearRateOfReturn}%</div>
+      <img className='img_Stock' src={stock} alt="" />
       <footer>
         更新 :
         {today.getFullYear()}年 /
